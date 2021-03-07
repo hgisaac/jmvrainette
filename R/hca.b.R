6ae1fb1b0581b838718fb7559b4d35cd245294e5
Hugo Isaac <hugo_iar@live.com>
2021-03-07 15:15:18 -0300

    Add R analysis file

---
+++ R/hca.b.R
@@ -0,0 +1,61 @@
+# This file is a generated template, your changes will not be overwritten
+
+hcaClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
+    "hcaClass",
+    inherit = hcaBase,
+    private = list(
+        .run = function() {
+            data <- self$data
+            row.names(data) <- paste0('row', 1:nrow(data))
+
+            data[[self$options$text]] <- as.character(
+                data[[self$options$text]]
+            )
+
+            corpus <- quanteda::corpus(data, text_field=self$options$text)
+            corpus <- rainette::split_segments(corpus, segment_size=self$options$segsize)
+
+            load(paste0('stopwords/', self$options$stopwords, '.Rdata'))
+
+            dtm <- quanteda::dfm(
+                corpus,
+                remove=stops, # TODO: stopwords::stopwords(self$options$stopwords),
+                tolower=self$options$tolower,
+                remove_punct=self$options$rmvpunct
+            )
+            dtm <- quanteda::dfm_wordstem(dtm, language=self$options$lang)
+            dtm <- quanteda::dfm_trim(dtm, min_termfreq=self$options$mintermfreq)
+            
+            results <- rainette::rainette(
+                dtm,
+                k=self$options$kmeans,
+                min_uc_size=self$options$minucsize,
+                min_split_members=self$options$minsplit
+            )
+
+            plotData <- list(results, dtm)
+            names(plotData) <- c('result', 'dtm')
+
+            image <- self$results$plot
+            image$setState(plotData)
+        },
+        .plot = function(image, ...) {
+            plotData <- image$state
+
+            plot <- rainette::rainette_plot(
+                plotData$result,
+                plotData$dtm,
+                k=self$options$kgroup,
+                type=self$options$plottype,
+                n_terms=self$options$nterms,
+                free_scales=self$options$freescales,
+                measure=self$options$measure,
+                show_negative=self$options$negative,
+                text_size=self$options$textsize
+            )
+
+            print(plot)
+            TRUE
+        }
+    )
+)
